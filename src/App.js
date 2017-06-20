01e8b742d82cb25f3dbd00f9ab81dcd4349bed00
Benjamin de Jesus <jdneb@hotmail.com>
2017-06-20 01:47:23 -0400

    colors and css

--- src/App.js
+++ src/App.js
@@ -6,22 +6,31 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      numChildren: 20
+      numChildren: 10,
+      nInterval: setInterval(()=>this.addSeed(), 100)
     }
-  }
+ }
 
-  componentWillMount(){
-    // setInterval(function(){
-    //   this.setState({
-    //   numChildren: this.state.numChildren+1
-    //   })
-    // }, 200)
-  }
+ addSeed(){
+  let newCount = this.state.numChildren + 1
+  let n = this.state.nInterval
+  this.setState(
+    {
+      numChildren: newCount
+    }
+  )
+  if (newCount >= 1000)
+  this.setState(
+    {
+      nInterval: clearInterval(n)
+    }
+  )
+ }
 
   makeSeeds(x){
     let answer = []
     for(let i=0; i<x; i++)
-          answer= answer.concat(<Seed key={i} x={i*2} y={i*2} i={i}/>)
+          answer= answer.concat(<Seed key={i} x={i*1.1} y={i*1.1} i={i}/>)
      return answer
   }
 
--- src/Components/Seed.js
+++ src/Components/Seed.js
@@ -1,34 +1,57 @@
 import React, { Component } from 'react';
 
+let colors = ['#222', '#444', '#666', '#888', '#aaa', '#ccc']
 class Seed extends Component {
   constructor(){
     super();
     this.state = {
-      children: 0,
-      angle   : 0,
-      x       : 0,
-      y       : 0
+      iterations: 0,
+      angle     : 0,
+      ratio     : 137.5,
+      x         : 0,
+      y         : 0,
+      bg        : 0
+      //,nInterval : setInterval(()=>this.returnToOrigin(),1000)
     }
-    //this.makeCounterShards = this.makeCounterShards.bind(this)
-    //this.decrement = this.decrement.bind(this)
   }
+
+  returnToOrigin(){
+  let newX = this.state.x * 137.5 / 360
+  let newY = this.state.y * 137.5 / 360
+  let n = this.state.nInterval
+  this.setState(
+    {
+      x: newX,
+      y: newY
+    }
+  )
+  if (newX <= 1.01)
+  this.setState(
+    {
+      nInterval: clearInterval(n)
+    }
+  )
+ }
 
   componentWillMount(){
     this.setState({
       x: this.props.x,
       y: this.props.y,
-      angle: this.props.i
+      angle: this.props.i,
+      bg: Math.floor(Math.random()*6)
     })
   }
 
   render() {
     return (
         <div className='seed' style={{
-          'transform': 'rotate(' + ((137.5 * this.state.angle) % 360) + 'deg)',
+          'backgroundColor': '#' + (Math.floor(this.state.angle / 50) + 1) * 111111,
+          'borderColor': '#' + (Math.floor(this.state.angle / 50)+1) * 111111,
+          'transform': 'rotate(' + ((this.state.ratio * this.state.angle) % 360) + 'deg)',
           'transformOrigin': -this.state.x + 'px ' + -this.state.y + 'px',
           'marginTop': this.state.y + 'px',
-          'marginLeft': this.state.x + 'px'
-
+          'marginLeft': this.state.x + 'px',
+          'animation': 'spin-right ' + (10 + .1 *  this.state.angle) + 's linear infinite alternate'
         }}>
         </div>
       );
